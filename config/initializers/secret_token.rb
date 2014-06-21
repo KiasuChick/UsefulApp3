4558979da49fda639c02cbfdf4511fc345135045
KiasuChick <me@kiasuchick.com>
2014-06-20 18:25:04 -0700

    basic controller, model, root route & psql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Usefulapp::Application.config.secret_key_base = '1c7aa519e20c4cfea2b164125d54ea42fc1607dba853f3a5fcb49db4af34c22b31b7f925652ac4d5003301023ae82fc71054ff155c8314e1168128e7f2ea68c3'
