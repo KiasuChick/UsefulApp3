d8d3ed8b72b28af38ba0933d3e3ec22173ae40f4
KiasuChick <me@kiasuchick.com>
2014-06-22 20:39:10 -0700

    added geocoder gem, did db command line migration for latlong

---
+++ db/migrate/20140623032755_add_longlat_to_locations.rb
@@ -0,0 +1,6 @@
+class AddLonglatToLocations < ActiveRecord::Migration
+  def change
+    add_column :locations, :longitude, :float
+    add_column :locations, :latitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621221734) do
+ActiveRecord::Schema.define(version: 20140623032755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20140621221734) do
     t.string   "store_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "longitude"
+    t.float    "latitude"
   end
 
 end
